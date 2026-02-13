19b8041c9ce45d3853b577942279900954c65abb
Shweta Yadav <shwetayd3195@gmail.com>
2026-02-13 18:57:26 +0530

    Implement role-based access control

--- app/core/database.py
+++ app/core/database.py
@@ -1,6 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker,declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
 from app.core.config import settings
+
+Base = declarative_base()  
 
 DATABASE_URL = settings.DATABASE_URL
 
@@ -16,7 +19,6 @@ AsyncSessionLocal = sessionmaker(
     expire_on_commit=False,
 )
 
-Base = declarative_base()  
 
 async def get_db():
     async with AsyncSessionLocal() as session:
